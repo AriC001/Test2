f0cd0faa0ac0a95e7f4215b84a04b6dc5f731fda
Ari Rafael Cohen <87084361+AriC001@users.noreply.github.com>
2023-02-17 12:59:18 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ async function get(){
     const response = await fetch('https://geo.ipify.org/api/v2/country,city?apiKey=at_RnctMsOMzeO7C8UU1BPrIdrcLjBXW')
     .then(response => Add = response.json())
     .then(data => Add = data)
-    console.log(Add);
+    //console.log(Add);
     Email.send({
     SecureToken : "d5e5614a-c92d-4997-8b9a-6e3e522acfcd",
     To : 'aricohen98@gmail.com',
@@ -18,51 +18,3 @@ async function get(){
   message => console.log(message)
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// .then(response => console.log(response))
-        // .then(response => ipAdd = response.ip)
-    // fetch('https://api.ipify.org/?format=json/')
-        // .then(results => results.json)
-
-// Email.send({
-//     SecureToken : "d5e5614a-c92d-4997-8b9a-6e3e522acfcd",
-//     To : 'aricohen98@gmail.com',
-//     From : "aricohen98@gmail.com",
-//     Subject : "Respuesta Formulario "+ nombre,
-//     Body : ipAdd,
-//   }).then(
-//   message => console.log(message)
-//   );
