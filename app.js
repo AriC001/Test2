38425fa69dbfedac7a8c5dfbfb0a207b5509bc95
Ari Rafael Cohen <87084361+AriC001@users.noreply.github.com>
2023-04-05 12:26:26 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
+const formWork = document.querySelector(".form_section2");
+const fileAdd = document.querySelector("#cvFiles");
 
 let Add;
 
-async function get(){
-    event.preventDefault();
-    const nombre = document.querySelector('#nombre').value
-    const response = await fetch('https://geo.ipify.org/api/v2/country,city?apiKey=at_RnctMsOMzeO7C8UU1BPrIdrcLjBXW')
-    .then(response => Add = response.json())
-    .then(data => Add = data)
-    Email.send({
-    SecureToken : "d5e5614a-c92d-4997-8b9a-6e3e522acfcd",
-    To : 'kahaneary@gmail.com',
-    From : "formulario@verdegestion.com",
-    Subject : nombre,
-    Body : Add,
-  }).then(
-  message => console.log(message)
-  );
-  alert("Muchas Gracias");
-  location.reload()
-}
+let fileHandler = function(e){
 
+  let emailCV = "Adjunto CV " + document.getElementById("cv-email").value
+ 
+  const file = e.target.files[0];
+  const reader = new FileReader();
+  reader.onloadend = () => {
+      console.log(reader.result);
+      // Logs data:<type>;base64,wL2dvYWwgbW9yZ...
+      Email.send({
+            SecureToken : "d5e5614a-c92d-4997-8b9a-6e3e522acfcd", 
+            To : 'aricohen98@gmail.com',
+            From : "formulario@verdegestion.com",
+            Subject : "CV WEB " + document.getElementById("cv-email").value,
+            Body : emailCV,
+          Attachments : [
+          {
+            name : file.name,
+            data: reader.result,
+          }]
+          }).then(
+          message => console.log(message)
+          );
+  };
+  reader.readAsDataURL(file);
+}
