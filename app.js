2597e6165f7033267d4ad665f8bb7c6ccb14669d
Ari Rafael Cohen <aricohen98@gmail.com>
2023-04-05 13:02:24 -0300

    Form styles

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const formWork = document.querySelector(".form_section2");
 const fileAdd = document.querySelector("#cvFiles");
 
-let Add;
+
+function sendEmail(name){
+  event.preventDefault();
+  alert("Muchas Gracias")
+  location.reload()
+}
 
 let fileHandler = function(e){
 
-  let emailCV = "Adjunto CV " + document.getElementById("cv-email").value
+  //let emailCV = "Adjunto CV " + document.getElementById("cv-email").value
  
   const file = e.target.files[0];
   const reader = new FileReader();
@@ -16,6 +21,31 @@ let fileHandler = function(e){
             SecureToken : "d5e5614a-c92d-4997-8b9a-6e3e522acfcd", 
             To : 'aricohen98@gmail.com',
             From : "formulario@verdegestion.com",
+            Subject : "Photo form ",
+            Body : 'a',
+          Attachments : [
+          {
+            name : file.name,
+            data: reader.result,
+          }]
+          }).then(
+          message => console.log(message)
+          );
+  };
+  reader.readAsDataURL(file);
+}
+
+
+
+/* const file = e.target.files[0];
+  const reader = new FileReader();
+  reader.onloadend = () => {
+      console.log(reader.result);
+      // Logs data:<type>;base64,wL2dvYWwgbW9yZ...
+      Email.send({
+            SecureToken : "8944c40b-4e8c-4cbf-848d-0c7da4235591", 
+            To : 'aricohen98@gmail.com',
+            From : "panimagroup.sa@hotmail.com",
             Subject : "CV WEB " + document.getElementById("cv-email").value,
             Body : emailCV,
           Attachments : [
@@ -28,4 +58,4 @@ let fileHandler = function(e){
           );
   };
   reader.readAsDataURL(file);
-}
+} */
